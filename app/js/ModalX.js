9d71621a23fe2e5cc02055cf85d493e894b97ab8
Nikita <46563672+V1be-Official@users.noreply.github.com>
2019-05-23 23:06:26 +0300

    update 1.7v

--- app/js/ModalX.js
+++ app/js/ModalX.js
@@ -1,5 +1,56 @@
+let body = document.body;
+
+var ModalXAnimations = {
+
+    fadeIn: function(element, duration = 500) {
+        element.style.removeProperty('display');
+        let display = window.getComputedStyle(element).display;
+
+        if (display === 'none') 
+            display = 'block';
+
+        let opacity = getComputedStyle(element).opacity;
+        element.style.display = display;
+        element.style.transitionDuration = 0 + 'ms';
+        element.style.opacity = 0;
+        getComputedStyle(element).opacity;
+        element.style.transitionDuration = duration + 'ms';
+        element.style.transitionProperty = `opacity`;     
+        element.style.opacity = opacity;
+        setTimeout(function () {
+            element.style.removeProperty('transition-duration');
+            element.style.removeProperty('transition-property');
+        } , duration)
+    },
+    fadeOut: function(element, duration = 500) {
+        element.style.transitionProperty = `opacity`;
+        element.style.transitionDuration = duration + 'ms';
+        element.style.opacity = 0;
+        window.setTimeout(function () {
+            element.style.display = 'none';
+            element.style.removeProperty('opacity');
+            element.style.removeProperty('transition-duration');
+            element.style.removeProperty('transition-property');
+        }, duration)
+    },
+    fadeToggle: function (element, duration = 500) {
+
+        if (window.getComputedStyle(element).display === 'none') {
+
+            return this.slideDown(element, duration);
+
+        } else {
+
+            return this.slideUp(element, duration);
+        }
+    }
+}
+
 function makeNeutral() {
-    document.body.style.overflow = "hidden";
+    if( body.style.overflow == "hidden" )
+        body.style.overflow = null;
+    else
+        body.style.overflow = "hidden";
 }
 
 
@@ -22,21 +73,32 @@ class ModalX {
         this.text = properties.text || "";
         this.buttonText = properties.buttonText || "OK";
     }
-    alert() {
-        console.log(this);
+    alert(text = "Your message has been sent!", className="success", duration = 600, durationOut = 3000) {
+        let mainFrame = document.createElement('div');
+        mainFrame.className = "ModalX " + className;
+        let mainText = document.createElement('span');
+        mainText.className = "text";
+        mainText.innerHTML = text;
+        mainFrame.appendChild(mainText);
+        mainFrame.onclick = function() {
+            ModalXAnimations.fadeOut(mainFrame, duration * 0.3);
+            setTimeout(function() {
+                body.removeChild(mainFrame)}, duration * 0.3);
+        }
+        setTimeout(function() {
+            if(!body.mainFrame)
+                return false;
+            ModalXAnimations.fadeOut(mainFrame, duration);
+            setTimeout(function() {
+                body.removeChild(mainFrame)}, duration);
+
+        } , durationOut);
+
+        body.appendChild(mainFrame);
+        ModalXAnimations.fadeIn(mainFrame, duration);
     }
 }
 
-test = new ModalX({
-    name: "Alert",
-    func: "Success",
-    width: 480,
-    height: 130,
-    timer: 0,
-    class: "myClass",
-    id: "myId",
-    title: "Добро пожаловать!",
-    text: "Hello, it's me!",
-    buttonText: "Ok"
-});
+
+test = new ModalX();
 test.alert();
